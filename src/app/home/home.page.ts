3e645d38371dc34d94b284f29a57a60ca651b4c3
Catherine Beredo <catherine.beredo@g.batstate-u.edu.ph>
2022-07-19 14:19:06 +0800

    Update main menu page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +7,7 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
 
-  constructor(private router: Router) {}
-  mainmenu(){
-    this.router.navigate(['mainmenu']);
-  }
+  constructor() {}
+ 
 
 }
--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+  login(){
+    this.router.navigate(['login']);
+  }
 
   ngOnInit() {
   }
