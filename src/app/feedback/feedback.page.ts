b351e2dbb37a7598b83d5f1897f1eb5c83d8b372
Catherine Beredo <catherine.beredo@g.batstate-u.edu.ph>
2022-07-26 22:38:32 +0800

    Updated feedback page

--- src/app/feedback/feedback.page.ts
+++ src/app/feedback/feedback.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-feedback',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedbackPage implements OnInit {
 
-  constructor() { }
+  stars:number;
+  constructor(private router: Router) { 
+    this.stars=0;
+  }
+  mainmenu(){
+    this.router.navigate(['mainmenu'])
+  }
+  starClicked(i:number){
+    this.stars=i;
+  }
 
   ngOnInit() {
   }
